db72e1cb9f18edee30ba1a68943de703b7b51872
Loise-Karuma <loicekaruma98@gmail.com>
2022-08-13 00:40:02 +0300

    Databse Connection

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = false
 
 @app.route('/')
 def index():
